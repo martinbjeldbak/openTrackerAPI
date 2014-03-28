924a2955d8569f34909506831920ed25c2d80a79
Martin Madsen <martin2madsen@gmail.com>
2014-03-28 20:47:09 +0100

    More devise stuff

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,10 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  def index
+    respond_to do |format|
+      format.html { render text: 'hi!' }
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,15 @@
 OpenTracker::Application.routes.draw do
+  get "users/index"
+  get "users/show"
   devise_for :users
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  root 'welcome#index'
+  root 'application#index'
+
+  #devise_for :users
+  resources :users
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
