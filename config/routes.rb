f02e15e7075b3d8a9314cf0b11c8371be2f189b1
Martin Madsen <martin2madsen@gmail.com>
2014-04-05 01:06:56 +0200

    Clean up routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,8 @@
 OpenTracker::Application.routes.draw do
   devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
-  # The priority is based upon order of creation: first created -> highest priority.
-  # See how all your routes lay out with "rake routes".
 
-  # You can have the root of your site routed with "root"
   root 'application#index'
 
-  #devise_for :users
   resources :users
 
   namespace :api, defaults: {format: :json} do
