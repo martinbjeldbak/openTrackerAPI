2020501b81ad7d32fee4aeb49e2f16aa08a72968
Martin Madsen <martinbmadsen@gmail.com>
2014-08-22 21:20:04 +0200

    Add authentication token to users and let migration be reversable

--- db/migrate/20140822112123_user_name_cannot_be_nil.rb
+++ db/migrate/20140822112123_user_name_cannot_be_nil.rb
@@ -1,5 +1,9 @@
 class UserNameCannotBeNil < ActiveRecord::Migration
-  def change
+  def self.up
     change_column :users, :name, :string, null: false
+  end
+
+  def self.down
+    change_column :users, :name, :string, null: true
   end
 end
