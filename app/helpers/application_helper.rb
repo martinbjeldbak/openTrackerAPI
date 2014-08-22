af77b54f52918878109a7acae374fe65ad65d4ee
Martin Madsen <martinbmadsen@gmail.com>
2014-08-22 23:11:33 +0200

    Add link to latest release of plugin

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,7 @@
 module ApplicationHelper
   def latest_ot_release_path
     #"https://github.com/fapper/openTracker/releases/latest"
-    'https://github.com/fapper/openTracker/archive/v0.1.zip'
+    'https://github.com/fapper/openTracker/archive/v1.0.zip'
   end
 
   def fapper_twitter_path
